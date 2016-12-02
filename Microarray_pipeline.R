a69cdb5ea6f19907533c4922969c1f931d333416
Michael Morley <mmorley@mail.med.upenn.edu>
2016-12-02 11:27:48 -0500

    More Fixes

--- Microarray_pipeline.R
+++ Microarray_pipeline.R
@@ -15,12 +15,13 @@ library(ggplot2)
 require(SPIA)
 library(oligo)
 library(readr)
+library(topGO)
 
 
 ################################### Please Set the following Paramtrers ##################################
 #
 projectname='CCAM_array'
-
+MSigDB_path='~/dsdata/projects/data_public/MSigDB/'
 #
 ##########################################################################################################
 
@@ -30,11 +31,10 @@ rownames(pData)=pData$sample_name
 phenoData <- new("AnnotatedDataFrame", data=pData)
 
 
-
-if(pData$organism[1]=='human'){
+if(unique(pData$organism)=='human'){
   library(hugene20sttranscriptcluster.db)
   annotationdb=hugene20sttranscriptcluster.db
-}else if (pData$organism[1]=='mouse'){
+}else if (unique(pData$organism)=='mouse'){
   library(mogene20sttranscriptcluster.db)
   annotationdb=mogene20sttranscriptcluster.db
 }else{
@@ -87,28 +87,29 @@ fit2 <- contrasts.fit(fit, contrast.matrix)
 fit2 <- eBayes(fit2)
 
 ######## load and prepare all the MSigDB sets for camera ######
+######## load and prepare all the MSigDB sets for camera ######
 if(unique(pData$organism)=="human"){
-  load( '/fujfs/d1/projects/data_public/MSigDB/human_H_v5.rdata')
+  load(paste(MSigDB_path, 'human_H_v5.rdata',sep=''))
   h.indices <- ids2indices(Hs.H,genenames$ENTREZID)
-  load(' /fujfs/d1/projects/data_public/MSigDB/human_c2_v5.rdata')
+  load(paste(MSigDB_path, 'human_c2_v5.rdata', sep=''))
   c2.indices <- ids2indices(Hs.c2,genenames$ENTREZID)
-  load(' /fujfs/d1/projects/data_public/MSigDB/human_c3_v5.rdata')
+  load(paste(MSigDB_path, 'human_c3_v5.rdata',sep=''))
   c3.indices <- ids2indices(Hs.c3,genenames$ENTREZID)
-  load(' /fujfs/d1/projects/data_public/MSigDB/human_c4_v5.rdata')
-  c4.indices <- ids2indices(Hs.c4,genenames$ENTREZID)
-}else
-  {
-load( '/fujfs/d1/projects/data_public/MSigDB/mouse_H_v5.rdata')
-h.indices <- ids2indices(Mm.H,genenames$ENTREZID)
-load(' /fujfs/d1/projects/data_public/MSigDB/mouse_c2_v5.rdata')
-c2.indices <- ids2indices(Mm.c2,genenames$ENTREZID)
-load(' /fujfs/d1/projects/data_public/MSigDB/mouse_c3_v5.rdata')
-c3.indices <- ids2indices(Mm.c3,genenames$ENTREZID)
-load(' /fujfs/d1/projects/data_public/MSigDB/mouse_c4_v5.rdata')
-c4.indices <- ids2indices(Mm.c4,genenames$ENTREZID)
-load('/Users/bapoorva/Desktop/ANALYSIS/msigdb/mouse_GO.rdata')
-GO.indices <- ids2indices(Mm.GO,genenames$ENTREZID)
+  load(paste(MSigDB_path, 'human_c4_v5.rdata',sep=''))
+  GO.indices <- ids2indices(Hs.c4,genenames$ENTREZID)
+}else if(unique(pData$organism)=="mouse"){
+  load( paste(MSigDB_path, 'mouse_H_v5.rdata',sep=''))
+  h.indices <- ids2indices(Mm.H,genenames$ENTREZID)
+  load(paste(MSigDB_path, 'mouse_c2_v5.rdata',sep=''))
+  c2.indices <- ids2indices(Mm.c2,genenames$ENTREZID)
+  load(paste(MSigDB_path, 'mouse_c3_v5.rdata',sep=''))
+  c3.indices <- ids2indices(Mm.c3,genenames$ENTREZID)
+  load(paste(MSigDB_path, 'mouse_c4_v5.rdata',sep=''))
+  c4.indices <- ids2indices(Mm.c4,genenames$ENTREZID)
+  load(paste(MSigDB_path,'mouse_GO.rdata',sep=''))
+  GO.indices <- ids2indices(Mm.GO,genenames$ENTREZID)
 }
+
 
 
 ##################################################################
@@ -130,27 +131,26 @@ names(spia) <- contrastnames
 # names(gsea) <- contrastnames
 
 
-i=1
 
 for(i in 1:length(contrastnames)){
   print(contrastnames[i])
   limma[[contrastnames[i]]] <- Cleanup(topTable(fit2,coef=i,n=Inf,p.value=1)) 
-  topgo[[contrastnames[i]]] <- runTopGO(limma[[contrastnames[i]]])
+  topgo[[contrastnames[i]]] <- runTopGO(limma[[contrastnames[i]]],organism =unique(pData$organism))
   
-#   #for each limma data (corresponding to the contrast), run ReactomePA
-   k=limma[[contrastnames[i]]]
-#   genes = k$fc
-#   names(genes) = k$ENTREZID 
-#   genes = genes[complete.cases(names(genes))]
-#   genes = genes[unique(names(genes))] 
-#   ss=genes[order(-genes)]
-#   y <- gsePathway(ss,organism="mouse")
-#   gsea[[contrastnames[i]]]=y
-#   if(nrow(summary(y))>0){
-#     eplot[[contrastnames[i]]] <-summary(y)}
-#   else{
-#     eplot[[contrastnames[i]]] <- data.frame()
-#   }
+  #   #for each limma data (corresponding to the contrast), run ReactomePA
+  k=limma[[contrastnames[i]]]
+  #   genes = k$fc
+  #   names(genes) = k$ENTREZID 
+  #   genes = genes[complete.cases(names(genes))]
+  #   genes = genes[unique(names(genes))] 
+  #   ss=genes[order(-genes)]
+  #   y <- gsePathway(ss,organism="mouse")
+  #   gsea[[contrastnames[i]]]=y
+  #   if(nrow(summary(y))>0){
+  #     eplot[[contrastnames[i]]] <-summary(y)}
+  #   else{
+  #     eplot[[contrastnames[i]]] <- data.frame()
+  #   }
   
   #for each limma data (corresponding to the contrast), run SPIA
   limma_sel <- k[which(abs(k$fc) > 2 & k$adj.P.Val < 0.05),]
@@ -160,23 +160,14 @@ for(i in 1:length(contrastnames)){
     names(sig_genes) = limma_sel$ENTREZID 
     sig_genes = sig_genes[complete.cases(names(sig_genes))]
     sig_genes = sig_genes[unique(names(sig_genes))] 
-    spia[[contrastnames[i]]] <- spia(de=sig_genes, all=all_genes, organism="mmu")}
-  else{
+    spia[[contrastnames[i]]] <- spia(de=sig_genes, all=all_genes, organism=ifelse(unique(pData$organism)=='mouse',"mmu",'hsa'))
+  }else{
     spia[[contrastnames[i]]] <- data.frame()
   }
   
-  #run camera
-  res.h <- camera(voom, h.indices, design,contrast.matrix[,i],inter.gene.cor=0.01)
-  res.c2 <- camera(voom, c2.indices, design,contrast.matrix[,i],inter.gene.cor=0.01)
-  res.GO <- camera(voom, GO.indices, design,contrast.matrix[,i],inter.gene.cor=0.01)
-  #res.c3 <- camera(v, c3.indices, design,i,inter.gene.cor=0.01)
-  #res.c4 <- camera(v, c4.indices, design,i,inter.gene.cor=0.01)
-  camera[[contrastnames[i]]] <- list(Hallmark=list(camera_result=res.h,indices=h.indices),Curated=list(camera_result=res.c2,indices=c2.indices),GO=list(camera_result=res.GO,indices=GO.indices))
-}
-
 
 ############ Save list of results ##############
-  results <- list(eset=eset,limma=limma,camera=camera, topgo=topgo,spia=spia)
+results <- list(eset=eset,limma=limma,camera=camera, topgo=topgo,spia=spia)
 save(results,file=paste(projectname, ".RData",sep=''))
 
 
